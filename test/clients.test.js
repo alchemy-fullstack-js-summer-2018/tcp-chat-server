01b501565c65c96a3e67bfd099bef3e38efadaf2
rbtprograms <rbt61690@gmail.com>
2018-07-09 21:27:45 -0700

    gets all clients working

--- test/clients.test.js
+++ test/clients.test.js
@@ -4,25 +4,30 @@ const Clients = require('../lib/clients');
 describe('Clients', () => {
 
     const c1 = {};
-    // const c2 = {};
-    // const c3 = {};
+    const c2 = {};
+    const c3 = {};
     let clients = null;
     beforeEach(() => {
         clients = new Clients();
         clients.add(c1);
-        // clients.add(c2);
-        // clients.add(c3);
+        clients.add(c2);
+        clients.add(c3);
     });
 
     it('assigns names', () => {
-        assert.equal(c1.username, 'user1');
-        // assert.equal(c2.username, 'user2');
-        // assert.equal(c3.username, 'user3');
+        assert.equal(c1.userName, 'user1');
+        assert.equal(c2.userName, 'user2');
+        assert.equal(c3.userName, 'user3');
     });
 
     it('gets a client', () => {
-        const thisClient = clients.getClient(c1.username);
-        assert.deepEqual(thisClient, c1);
+        const thisClient = clients.getClient(c2.userName);
+        assert.deepEqual(thisClient, c2);
+    });
+
+    it('gets all the clients', () => {
+        const allClients = clients.all();
+        assert.deepEqual(allClients, [c1, c2, c3]);
     });
 
     // it('removes clients', () => {
