0cb2afa975c0d005ac062bf1770b6e4048a0b2b8
Rehmsy <sarahrehmer@gmail.com>
2018-07-10 12:05:01 -0700

    started processMessage

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = require('./lib/app');
 const PORT = 12345;
 
 app.on('listening', () => {
-    console.log('chat app started on port, yo', PORT);
+    // why does VS Code think console.log is a variable?
+    // console.log('chat app started on port, yo', PORT);
 
 });
 
