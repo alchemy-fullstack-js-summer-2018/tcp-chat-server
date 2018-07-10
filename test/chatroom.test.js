c9bc3a6af19d7ce78ba949d8e1dec63f288de728
MariahAdams <mariah.adams23@yahoo.com>
2018-07-10 10:14:52 -0700

    working on second test: getting all clients

--- test/chatroom.test.js
+++ test/chatroom.test.js
@@ -4,14 +4,25 @@ const ChatRoom = require('../lib/chatroom');
 describe('Chat Room', () => {
 
     const c1 = {};
+    const c2 = {};
+    const c3 = {};
     let clients = null;
 
     beforeEach(() => {
         clients = new ChatRoom();
         clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
     });
 
     it('adds a user with a username', () => {
         assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
+    });
+
+    it('adds usernames to client object', () => {
+        const allClients = clients.getAllClients;
+        assert.deepEqual(allClients, [c1, c2, c3]);
     });
 });
