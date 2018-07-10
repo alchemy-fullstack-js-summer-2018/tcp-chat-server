adf1cc9d08f3ab7b77421492a2e7ccb6fc2ecffb
Mark Myers <markalope@gmail.com>
2018-07-10 11:00:10 -0700

    wrote rename user test

--- test/parse-message-test.js
+++ test/parse-message-test.js
@@ -2,8 +2,18 @@ const assert = require('assert');
 const parseMessage = require('../lib/parseMessage');
 
 describe('parse message', () => {
+    
     it('returns null if the command does not begin with @ symbol', () => {
         const message = 'change my username';
         assert.equal(parseMessage(message), null);
+    });
+
+    it('handles the rename command @nick', () => {
+        const command = '@nick NewUserName';
+        const expected = {
+            command: '@nick',
+            arg: 'NewUserName',
+        };
+        assert.deepEqual(parseMessage(command), expected);
     });
 });
