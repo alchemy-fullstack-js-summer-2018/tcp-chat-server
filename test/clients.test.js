1bc6e7959497f45bd57ace66d3a3d45ed6068e3e
carriecarter <carrie@carrie-carter.com>
2018-07-10 13:25:14 -0700

    woops.. canvas submit

--- test/clients.test.js
+++ test/clients.test.js
@@ -14,14 +14,10 @@ describe('Clients', () => {
         clients.add(c3);
     });
     
-    it('returns a name as the username property', () => {
+    it('assigns the username as a property of the client object', () => {
         assert.equal(c1.username, 'user1');
         assert.equal(c2.username, 'user2');
         assert.equal(c3.username, 'user3');
-    });
-
-    it('assigns the username as a property of the client object', () => {
-
     });
 
     it('returns an identical object as the one supplied to the add call', () => {
