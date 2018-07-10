4aa784a89fb4359f5f55383e1be50ddf8655de46
Mark Myers <markalope@gmail.com>
2018-07-10 08:56:35 -0700

    think I finished chatroon creation test

--- test/ChatRoom-test.js
+++ test/ChatRoom-test.js
@@ -15,4 +15,15 @@ describe('ChatRoom', () => {
         clients.add(c3);
     });
     
+    it('assigns a username to a client/socket and saves', () => {
+        assert.equal(c1.username, 'guest1');
+        assert.equal(c2.username, 'guest2');
+        assert.equal(c3.username, 'guest3');
+        assert.deepEqual(clients.getClient(c1.username), c1);
+    });
+
+    it('returns list of all clients', () => {
+        assert.deepEqual(clients.all(), [c1, c2, c3]);
+    });
+
 });
