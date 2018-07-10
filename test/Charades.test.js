a4373e3c6d4233a037090a5261360ae929f2db2d
Arthur Jen <arthurljen@gmail.com>
2018-07-10 12:11:57 -0700

    charade tests

---
+++ test/Charades.test.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const Charades = require('../lib/Charades');
+
+describe.only('Charades', () => {
+    let charades = null;
+    let player = 'jim';
+    beforeEach(() => {
+        charades = new Charades();
+    });
+
+    it('starts game', () => {
+        charades.start(player);
+        assert.ok(charades.gameInProgress);
+        assert.ok(charades.actor);
+        assert.ok(charades.secretWord);
+    });
+
+    it('takes guesses', () => {
+
+    });
+
+    it('ends game', () => {
+
+    });
+});
