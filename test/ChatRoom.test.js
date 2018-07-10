1d0e4041498b78ae359125606d33712712ea035d
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-09 18:14:29 -0700

    Begin implementing getClient method

--- test/ChatRoom.test.js
+++ test/ChatRoom.test.js
@@ -3,14 +3,24 @@ const ChatRoom = require('../lib/ChatRoom');
 
 describe('Chat room', () => {
     const c1 = {};
+    const c2 = {};
+    const c3 = {};
     let chatRoom = null;
 
     beforeEach(() => {
         chatRoom = new ChatRoom();
         chatRoom.add(c1);
+        chatRoom.add(c2);
+        chatRoom.add(c3);
     });
 
-    it('takes a socket and assigns a username', () => {
+    it('assigns a username', () => {
         assert.equal(c1.username, 'user1');
+        assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
+    });
+    it('gets the username', () => {
+        let client = chatRoom.getClient('user1');
+        assert.equal(client, 'username');
     });
 });
