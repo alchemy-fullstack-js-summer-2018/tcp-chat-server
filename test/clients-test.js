ffd3994c0981b28c4954954a7bdb9ad9fe7bdde6
Rehmsy <sarahrehmer@gmail.com>
2018-07-10 10:40:56 -0700

    getAll and remove tests failing then pass

--- test/clients-test.js
+++ test/clients-test.js
@@ -4,16 +4,30 @@ const Clients = require('../lib/clients');
 describe('Clients', () => {
     const c1 = {};
     const c2 = {};
+    const c3 = {};
     let clients = null;
     beforeEach(() => {
         clients = new Clients();
         clients.add(c1);
         clients.add(c2);
-    
+        clients.add(c3);
     });
 
     it('assigns namez', () => {
         assert.equal(c1.username, 'user1');
         assert.equal(c2.username, 'user2');
+        assert.equal(c3.username, 'user3');
     });
+
+    it('stores all active clients', () => {
+        const allClients = clients.getAllClients();
+        assert.deepEqual(allClients, [c1, c2, c3]);
+    });
+
+    it('removes a client', () => {
+        clients.remove(c1);
+        const allClients = clients.getAllClients();
+        assert.deepEqual(allClients, [c2, c3]);
+    });
+
 });
