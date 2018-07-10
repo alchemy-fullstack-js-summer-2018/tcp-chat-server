3cd34d1263715c2e889440f6e7f3532bd1c13660
Easton Gorishek <ieaston26@gmail.com>
2018-07-10 10:04:04 -0700

    rename and all method done

--- test/clients.test.js
+++ test/clients.test.js
@@ -2,40 +2,49 @@ const assert = require('assert');
 const ChatRoom = require('../lib/chatRoom');
 
 describe('Chatroom clients', () => {
-    let clients = null;
+    let chatRoom = null;
     const c1 = {};
     const c2 = {};
     const c3 = {};
 
     beforeEach(() => {
-        clients = new ChatRoom();
-        clients.add(c1);
-        clients.add(c2);
-        clients.add(c3);
-        console.log(c1);
+        chatRoom = new ChatRoom();
+        chatRoom.add(c1);
+        chatRoom.add(c2);
+        chatRoom.add(c3);
     });
     
-    it('takes new sockets and assigns usernames', () => {
-        assert.equal(c1.userName, 'user-1');
-        assert.equal(c2.userName, 'user-2');
-        assert.equal(c3.userName, 'user-3');
+    it('assigns username property to client object', () => {
+        assert.equal(c1.username, 'user-1');
+        assert.equal(c2.username, 'user-2');
+        assert.equal(c3.username, 'user-3');
     });
 
-    it('stores client', () => {
-        const client = clients.getClient(c1.userName);
+    it('returns same name that was supplied to the add method', () => {
+        const client = chatRoom.getClient(c1.username);
         assert.deepEqual(client, c1);
     });
 
-    // it('it renames a username', () => {
+    it('it renames a user', () => {
+        const client = c1.username;
+        const newName = 'Big-Mouth';
+        const newusername = chatRoom.rename(client, newName);
+        assert.equal(newusername, true);
+        assert.equal(chatRoom.getClient(client), undefined);
+        assert.ok(chatRoom.getClient(newName));
+        assert.equal(chatRoom.getClient(newName).username, newName);
+    });
 
-    // });
+    it('does not allow user to rename to existing username', () => {
+        const result = chatRoom.rename(c1.username, c2.username);
+        assert.equal(result, false);
+        assert.equal(c1, chatRoom.getClient(c1.username));
+        assert.equal(c2, chatRoom.getClient(c2.username));
+    });
 
-    // it('cannot rename to an existing username', () => {
-
-    // });
-
-    // it('returns total list of clients', () => {
-
-    // });
+    it('returns total list of clients', () => {
+        const result = chatRoom.all();
+        assert.deepEqual(result, [c1, c2, c3]);
+    });
 
 });
