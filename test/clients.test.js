3fe332d41b8002466261d18c3c222b9a65fcf3c4
MariahAdams <mariah.adams23@yahoo.com>
2018-07-10 09:26:44 -0700

    started in the wrong file, switching to client js for testing

---
+++ test/clients.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const ChatRoom = require('../lib/chatroom');
+
+describe('Chat Room', () => {
+
+    const c1 = {};
+    let clients = null;
+
+    beforeEach(() => {
+        clients = new clients();
+        clients.add(c1);
+    });
+
+    it('adds a user with a username', () => {
+       
+    });
+});
