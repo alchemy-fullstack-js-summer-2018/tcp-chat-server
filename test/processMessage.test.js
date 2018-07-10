6c30764d68d64020be428f19a27d0782683a66c2
Mario Quintana <pyrros2000@gmail.com>
2018-07-10 10:40:53 -0700

    processMessage test passing but it's a false-positive

--- test/processMessage.test.js
+++ test/processMessage.test.js
@@ -1,6 +1,13 @@
 const assert = require('assert');
 const processMessage = require('../lib/processMessage');
 
-describe('the message starting with @', () => {
-    assert.equal(processMessage('@hello'), 'HELLO');
+describe('messages starting with @', () => {
+
+    it('Message with @', () => {
+        assert.equal(processMessage('@hello'), 'HELLO');
+    });
+
+    it('Message without @', () => {
+        assert.equal(processMessage('hello'), 'hello');
+    });
 });
