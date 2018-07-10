c13dcd34bd9210c2b01969bd2690308cc9bc42a1
Robyn Navarro <rnavarro182@gmail.com>
2018-07-09 22:07:12 -0700

    untested parseMessage function done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./lib/app');
 const PORT = 15678;
 
 app.on('listening', () => {
-    console.log('TCP server listening on port', PORT);
+    console.log('chat app started on port', PORT);
 });
 
 app.listen(PORT);
