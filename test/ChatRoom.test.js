33e00f478565e0d61a88f8932719b19975e45209
Arthur Jen <arthurljen@gmail.com>
2018-07-09 19:17:44 -0700

    successfully renaming users

--- test/ChatRoom.test.js
+++ test/ChatRoom.test.js
@@ -7,6 +7,7 @@ describe.only('ChatRoom', () => {
     const u2 = {};
     const u3 = {};
     let users = null;
+    
     beforeEach(() => {
         users = new ChatRoom();
         users.add(u1);
@@ -14,12 +15,17 @@ describe.only('ChatRoom', () => {
         users.add(u3);
     });
 
-    it('assigns random username to a socket and stores by user name', () => {
+    it('assigns generic username to a socket and stores by user name', () => {
         
-        assert.equal(u1.username, 'user1');
-        assert.equal(u2.username, 'user2');
-        assert.equal(u3.username, 'user3');
+        assert.equal(u1.username, 'anonymous1');
+        assert.equal(u2.username, 'anonymous2');
+        assert.equal(u3.username, 'anonymous3');
 
+        assert.deepEqual(users.getClient(u1.username), u1);
+    });
+
+    it('renames a user', () => {
+        users.rename(u1.username, 'arthur');
         assert.deepEqual(users.getClient(u1.username), u1);
     });
 
