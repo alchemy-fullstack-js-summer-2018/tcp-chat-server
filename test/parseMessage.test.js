a2d7c8563f8fda970e39fb675ca8a415060c4e84
Robyn Navarro <rnavarro182@gmail.com>
2018-07-10 09:26:30 -0700

    all parseMessage tests pass, for now...

--- test/parseMessage.test.js
+++ test/parseMessage.test.js
@@ -2,8 +2,40 @@ const assert = require('assert');
 const parseMessage = require('../lib/parseMessage');
 
 describe('parsing messages', () => {
+    const invalidMessage = 'Hello anyone!';
+    const publicMessage = '@all Hello everyone!';
+    const rename = '@nick:new_name ';
+    const privateMessage = '@dm:some_user do you have the package?';
+    const allCommand = {
+        command: 'all',
+        text: 'Hello everyone!'
+    };
+    const nickCommand = {
+        command: 'nick',
+        arg: 'new_name'
+    };
+    const dmCommand = {
+        command: 'dm',
+        arg: 'some_user',
+        text: 'do you have the package?'
+    };
+
+
     it('returns null if message does not begin with @', () => {
-        const message = 'Hello everyone!';
-        assert.equal(parseMessage(message), null);
+        assert.equal(parseMessage(invalidMessage), null);
     });
+
+    it('returns command object with all and text', () => {
+        assert.deepEqual(parseMessage(publicMessage), allCommand);
+    });
+
+    it('returns command object with nick and new_name', () => {
+        assert.deepEqual(parseMessage(rename), nickCommand);
+    });
+
+    it('returns command object with dm, some_user and text', () => {
+        assert.deepEqual(parseMessage(privateMessage), dmCommand);
+    });
+
+
 });
