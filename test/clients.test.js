512979e7710a1eadc868e61bfa6ef327720d6bf3
Mario Quintana <pyrros2000@gmail.com>
2018-07-10 09:32:35 -0700

    test passing for adding clients and assigning names

--- test/clients.test.js
+++ test/clients.test.js
@@ -2,5 +2,23 @@ const assert = require('assert');
 const Clients = require('../lib/clients');
 
 describe('the clients', () => {
-    
-})
+
+    const c1 = {};
+    const c2 = {};
+    const c3 = {};
+    let clients = null;
+    beforeEach(() => {
+        clients = new Clients();
+        clients.add(c1);
+        clients.add(c2);
+        clients.add(c3);
+
+    });
+
+    it('Assign names', () => {
+        assert.equal(c1.username, 'user1');
+        assert.equal(c1.username, 'user1');
+        assert.equal(c1.username, 'user1');
+    });
+
+});
