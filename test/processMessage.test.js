f5a8454aeaca4cea2fafe9944a5192b6d58e81c8
Mario Quintana <pyrros2000@gmail.com>
2018-07-15 20:27:44 -0700

    created client.js file that deifines the sockets and creates interface

--- test/processMessage.test.js
+++ test/processMessage.test.js
@@ -3,14 +3,23 @@ const processMessage = require('../lib/processMessage');
 
 describe('messages in chatRoom', () => {
     const invalid = 'hello';
-    const valid = '@hello';
+    const valid = '@all hello';
+    const expected = {
+        command: 'all',
+        arg: undefined,
+        text: 'hello'
+    };
 
     it('Message starts with @', () => {
-        assert.ok(processMessage(valid), '@');
+        assert.ok(processMessage(valid), '@all');
     });
 
     it('Message does not start with @', () => {
         assert.equal(processMessage(invalid), null);
+    });
+
+    it('returns object', () => {
+        assert.deepEqual(processMessage(valid), expected); 
     });
     
 });
